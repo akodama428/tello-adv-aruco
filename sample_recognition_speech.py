835b119ff627f8e9ccf12bd123f43e4949d5f18e
akodama <kodamaatsushi428@gmail.com>
2023-10-18 19:58:57 +0900

    音声入力機能を追加

---
+++ sample_recognition_speech.py
@@ -0,0 +1,158 @@
+import speech_recognition as sr
+from DroneBlocksTelloSimulator.DroneBlocksSimulatorContextManager import DroneBlocksSimulatorContextManager
+import os
+import openai
+import pyttsx3
+import re
+
+## 元ネタ
+## https://qiita.com/Nekonun/items/2de0d5b3c77206c5ba31
+
+## ここにOpenAIのAPI Keyを入力
+## https://platform.openai.com/account/api-keys
+OPENAI_API_KEY = "sk-***"
+
+##############
+# 音声認識関数 #
+##############
+def recognize_speech():
+
+    recognizer = sr.Recognizer()    
+    # Set timeout settings.
+    recognizer.dynamic_energy_threshold = False
+
+    
+    with sr.Microphone() as source:
+        recognizer.adjust_for_ambient_noise(source)
+    
+        while(True):
+            print(">> Please speak now...")
+            audio = recognizer.listen(source, timeout=1000.0)
+
+            try:
+                # Google Web Speech API を使って音声をテキストに変換
+                text = recognizer.recognize_google(audio, language="ja-JP")
+                print("[You]")
+                print(text)
+                return text
+            except sr.UnknownValueError:
+                print("Sorry, I could not understand what you said. Please speak again.")
+                #return ""
+            except sr.RequestError as e:
+                print(f"Could not request results; {e}")
+                #return ""
+
+
+#################################
+# Pyttsx3でレスポンス内容を読み上げ #
+#################################
+def text_to_speech(text):
+    # テキストを読み上げる
+    engine.say(text)
+    engine.runAndWait()
+
+def chat(conversationHistory):
+    # APIリクエストを作成する
+    response = openai.ChatCompletion.create(
+        messages=conversationHistory,
+        max_tokens=1024,
+        n=1,
+        stream=True,
+        temperature=0.5,
+        stop=None,
+        presence_penalty=0.5,
+        frequency_penalty=0.5,
+        model="gpt-3.5-turbo"
+    )
+
+    # ストリーミングされたテキストを処理する
+    fullResponse = ""
+    RealTimeResponce = ""   
+    for chunk in response:
+        text = chunk['choices'][0]['delta'].get('content')
+
+        if(text==None):
+            pass
+        else:
+            fullResponse += text
+            RealTimeResponce += text
+            print(text, end='', flush=True) # 部分的なレスポンスを随時表示していく
+
+            target_char = ["。", "！", "？", "\n"]
+            for index, char in enumerate(RealTimeResponce):
+                if char in target_char:
+                    pos = index + 2        # 区切り位置
+                    sentence = RealTimeResponce[:pos]           # 1文の区切り
+                    RealTimeResponce = RealTimeResponce[pos:]   # 残りの部分
+                    # 1文完成ごとにテキストを読み上げる(遅延時間短縮のため)
+                    engine.say(sentence)
+                    engine.runAndWait()
+                    break
+                else:
+                    pass
+
+    # APIからの完全なレスポンスを返す
+    return fullResponse
+
+
+##############
+# メインの関数 #
+##############
+if __name__ == '__main__':
+
+    ##################
+    # ChatGPTの初期化 #
+    ##################
+    openai.api_key = OPENAI_API_KEY
+    # UserとChatGPTとの会話履歴を格納するリスト
+    conversationHistory = []
+    # setting = {"role": "system", "content": "句読点と読点を多く含めて応答するようにして下さい。また、1文あたりが長くならないようにして下さい。"}
+    setting = {"role": "system", "content": "あなたはドローンを制御する操縦士です。離陸してほしいとお願いされたら一言チャットで回答した後に「takeoff」をつけてださい"}
+    conversationHistory.append(setting)
+    # chat(conversationHistory)
+    
+    ##################
+    # Pyttsx3を初期化 #
+    ##################
+    engine = pyttsx3.init()
+    # 読み上げの速度を設定する
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate', rate)
+    # Kyokoさんに喋ってもらう(日本語)
+    engine.setProperty('voice', "com.apple.ttsbundle.Kyoko-premium")
+
+    #######################
+    # DroneSimulator初期化 #
+    #######################
+    sim_key = 'de241945-b0a8-4151-b611-3d45fca5d99d'
+    distance = 40
+
+    # Ctrl-Cで中断されるまでChatGPT音声アシスタントを起動
+    while True:
+        with DroneBlocksSimulatorContextManager(simulator_key=sim_key) as drone:
+
+            # 音声認識関数の呼び出し
+            text = recognize_speech()
+
+            if text:
+                print(" >> Waiting for response from ChatGPT...")
+                # ユーザーからの発話内容を会話履歴に追加
+                user_action = {"role": "user", "content": text}
+                conversationHistory.append(user_action)
+
+                print("[ChatGPT]") #応答内容をコンソール出力
+                res = chat(conversationHistory)
+
+                # ChatGPTからの応答内容を会話履歴に追加
+                chatGPT_responce = {"role": "assistant", "content": res}
+                conversationHistory.append(chatGPT_responce) 
+                # print(conversationHistory)
+
+                if res.count("takeoff") >0:
+                    drone.takeoff()
+                    drone.fly_forward(distance, 'in')
+                    drone.fly_left(distance, 'in')
+                    drone.fly_backward(distance, 'in')
+                    drone.fly_right(distance, 'in')
+                    drone.flip_backward()
+                    drone.land()
